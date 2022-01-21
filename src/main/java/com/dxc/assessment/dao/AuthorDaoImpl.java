56f52bbc9f1e67f0ec1478725b8a24527fc7d303
TelluriSireesha <96856497+TelluriSireesha@users.noreply.github.com>
2022-01-20 22:09:09 -0800

    ASSESSMENT

--- src/main/java/com/dxc/assessment/dao/AuthorDaoImpl.java
+++ src/main/java/com/dxc/assessment/dao/AuthorDaoImpl.java
@@ -1,12 +1,43 @@
 package com.dxc.assessment.dao;
 
 import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import javax.sql.DataSource;
 import com.dxc.assessment.modal.Author;
 
-public class AuthorDaoImpl implements AuthorDao{
+public class AuthorDaompl implements AuthorDao{
+    private DataSource dataSource;
+
+    private static final String INSERT_ONE_AUTHOR;
+
+    private static final String SELECT_ALL_AUTHOR;
+
+
+    static {
+        INSERT_ONE_AUTHOR = "INSERT INTO authors (id, first_name, last_name, genre, email) VALUES (?, ?, ?)";
+        SELECT_ALL_AUTHOR = "SELECT * FROM authors";
+    }
 
     @Override
-    public Author create(Author author) {
+    public int save(Author author) throws SQLException {
+        Connection connection = dataSource.getConnection();
+        PreparedStatement ps = connection.prepareStatement(INSERT_ONE_AUTHOR);
+        ps.setString(1, author.getId());
+        ps.setString(2, author.getFirst_Name());
+        ps.setString(3, author.getLast_Name());
+        ps.setString(3, author.getGenre());
+        ps.setString(3, author.getEmail());
+        int rows = 0;
+        rows = ps.executeUpdate();
+
+        ps.close();
+        connection.close();
+        return rows;
         return null;
     }
 
@@ -21,8 +52,22 @@ public class AuthorDaoImpl implements AuthorDao{
     }
 
     @Override
-    public List<Author> findAll() {
-        return null;
+    public List<Author> findAll() throws SQLException {
+    
+        Connection connection = dataSource.getConnection();
+        List<Author> authors;
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery(SELECT_ALL_AUTHOR);
+        authors = new ArrayList();
+        while (rs.next()) {
+            authors.add(new Author(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5)));
+        }
+        rs.close();
+        statement.close();
+        connection.close();
+        return authors;
     }
+    return null;
+}
     
 }
